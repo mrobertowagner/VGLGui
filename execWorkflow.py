1225a3d18e650207d2b71ec903967d3aa5d4d7e5
mrobertowagner <mrobertowagnermaciel@gmail.com>
2021-08-19 15:33:14 -0300

    v3

--- execWorkflow.py
+++ execWorkflow.py
@@ -109,23 +109,23 @@ for vGlyph in lstGlyph:
     elif vGlyph.func == 'ShowImage':
 
         # Returns edge image based on glyph id
-        #ShowImage_img_input = getImageInputByIdName(vGlyph.glyph_id, 'image')
+        ShowImage_img_input = getImageInputByIdName(vGlyph.glyph_id, 'image')
 
-        vl.vglCheckContext(vglClThreshold_img_output,vl.VGL_RAM_CONTEXT())
-        img_thresh = VglImage.get_ipl(vglClThreshold_img_output)
-        imshow(img_thresh)
+        #vl.vglCheckContext(vglClThreshold_img_output,vl.VGL_RAM_CONTEXT())
+        #img_thresh = VglImage.get_ipl(vglClThreshold_img_output)
+        #imshow(img_thresh)
 
-        vl.vglCheckContext(vglClBlurSq3_img_output,vl.VGL_RAM_CONTEXT())
-        img_blur = VglImage.get_ipl(vglClBlurSq3_img_output)
-        imshow(img_blur)
+        #vl.vglCheckContext(vglClBlurSq3_img_output,vl.VGL_RAM_CONTEXT())
+        #img_blur = VglImage.get_ipl(vglClBlurSq3_img_output)
+        #imshow(img_blur)
 
-        #if ShowImage_img_input is not None:
+        if ShowImage_img_input is not None:
 
             # Rule3: In a sink glyph, images (one or more) can only be input parameters             
-            #ShowImage_img_input.show()
+            ShowImage_img_input.show()
 
             # Actions after glyph execution
-            #GlyphExecutedUpdate(vGlyph.glyph_id, None)
+            GlyphExecutedUpdate(vGlyph.glyph_id, None)
 
     elif vGlyph.func == 'vglSaveImage':
 
